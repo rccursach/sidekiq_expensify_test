c532e8e3b631de71fbc3f35d5ff657d95317f11f
Ricardo Carrasco CUrsach <rccursach@gmail.com>
2017-01-19 19:39:06 -0300

    config file for sidekiq

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,6 +1,6 @@
 class TestWorker
   include Sidekiq::Worker
-
+  sidekiq_options :queue => :test, :retry => false, :backtrace => false
   def perform(partner_id, partner_secret)
     puts 'SIDEKIQ WORKER RUNNING TEST TASK'
   end
